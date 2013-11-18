661986c1ca200eb69bd78e99700c5525162d3f11
Fergus McDowall <fergusmcdowall@Ferguss-MacBook-Air.local>
2013-11-18 20:19:10 +0100

    trimmed test files, use forage-indexer package instead

--- test/testdata/index.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-FILTERFLAG=" --form filterOn=places,topics,organisations"
-
-for OUTPUT in $(ls *.json)
-do
-    curl --form document=@$OUTPUT http://localhost:3000/indexer $FILTERFLAG
-done
