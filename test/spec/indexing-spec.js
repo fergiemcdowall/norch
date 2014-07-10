55d622d624e63eb130f88e26551c565c419b70bd
fergiemcdowall <fergusmcdowall@gmail.com>
2014-07-10 15:27:19 +0200

    new test framework that actually works, tests need to be fleshed out

---
+++ test/spec/indexing-spec.js
@@ -0,0 +1,17 @@
+var fs = require('fs');
+var si = require('../../lib/forage.js');
+
+describe('indexing and search', function () {
+
+  //THIS IS A PLACEHOLDER:
+  //TODO: MAKE TESTS REAL
+
+  var data = fs.readFileSync('test/testdata/reuters-000.json');
+
+  it('should index one file of test data', function () {
+    expect(true).toEqual(true);
+  });
+
+
+});
+
