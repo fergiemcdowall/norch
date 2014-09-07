88e038c74a61a65f34cc87c6a14f51a42e3af220
Fergus McDowall <fergusmcdowall@gmail.com>
2014-09-07 09:15:44 +0200

    bumping versions, test data as a devdependency

--- test/spec/indexing-spec.js
+++ test/spec/indexing-spec.js
@@ -6,7 +6,7 @@ describe('indexing and search', function () {
   //THIS IS A PLACEHOLDER:
   //TODO: MAKE TESTS REAL
 
-  var data = fs.readFileSync('test/testdata/reuters-000.json');
+  var data = fs.readFileSync('node_modules/reuters-21578-json/data/reuters-000.json');
 
   it('should index one file of test data', function () {
     expect(true).toEqual(true);
