08522f318528e85cbfaf84107be9a2a6af348c52
Fergus McDowall <fergusmcdowall@gmail.com>
2021-02-22 09:42:00 +0100

    added skeleton swagger api

---
+++ src/index.js
@@ -0,0 +1,75 @@
+const url = require('url')
+
+module.exports = (index, sendResponse) => {
+
+  const params = _url => url.parse(_url, {
+    parseQueryString: true
+  }).query
+
+  const sendJSONResponse = (body, res) => {
+    res.setHeader("Content-Type", 'application/json; charset=utf-8')
+    res.writeHead(200)
+    res.end(JSON.stringify(body, null, 2))
+  }
+  
+  // *************
+  
+  // curl -H "Content-Type: application/json" --data @testdata.json http://localhost:8081/put
+  // if ?form=[fieldName] then read from that form field
+  const put = (req, res) => {
+    const formField = url.parse(req.url, {
+      parseQueryString: true
+    }).query.form
+    var body = ""
+    req.on('data', d => body += d.toString())
+    req.on('end', () => index.PUT(JSON.parse(
+      formField
+        ? querystring.decode(body)[formField]
+        : body
+    )).then(idxRes => {
+      sendJSONResponse(idxRes, res)
+    }))
+  }
+
+  const get = (req, res) => res.end('This is GET yo!')
+
+  const allDocuments = (req, res) => {
+    debugger
+    return index.ALL_DOCUMENTS(
+      +params(req.url).limit || undefined
+    ).then(ad => sendJSONResponse(ad, res))
+  }
+
+  const buckets = (req, res) => index.BUCKETS(
+    ...JSON.parse(params(req.url).q)
+  ).then(b => sendJSONResponse(b, res))
+
+  const documentCount = (req, res) => index.DOCUMENT_COUNT().then(
+    td => sendResponse(td + '', res, 'text/plain')
+  )
+
+  const created = (req, res) => index.CREATED().then(
+    c => sendResponse(c + '', res, 'text/plain')
+  )
+
+  const lastUpdated = (req, res) => index.LAST_UPDATED().then(
+    lu => sendResponse(lu + '', res, 'text/plain')
+  )
+
+  const query = (req, res) => index.QUERY(
+    params(req.url).q,
+    JSON.parse(params(req.url).ops)
+  ).then(r => sendJSONResponse(r, res))
+  
+  return {
+    put: put,
+    get: get,
+    allDocuments: allDocuments,
+    buckets: buckets,
+    documentCount: documentCount,
+    created: created,
+    lastUpdated: lastUpdated,
+    query: query
+  }
+
+}
--- src/main.js
+++ src/main.js
@@ -4,77 +4,15 @@ const mime = require('mime')
 const path = require('path')
 const querystring = require('querystring')
 const si = require('search-index')
-const url = require('url')
+const Index = require('./index.js')
 
 
-// curl -H "Content-Type: application/json" --data @testdata.json http://localhost:8081/put
-// if ?form=[fieldName] then read from that form field
-const put = (req, res, index) => {
-  const formField = url.parse(req.url, {
-    parseQueryString: true
-  }).query.form
-  var body = ""
-  req.on('data', d => body += d.toString())
-  req.on('end', () => index.PUT(JSON.parse(
-    formField
-      ? querystring.decode(body)[formField]
-      : body
-  )).then(idxRes => {
-    sendJSONResponse(idxRes, res)
-  }))
-}
-
-const get = (req, res) => res.end('This is GET yo!')
-
-
-const params = _url => url.parse(_url, {
-  parseQueryString: true
-}).query
-
-const allDocuments = (req, res, index) => index.ALL_DOCUMENTS(
-  +params(req.url).limit || undefined
-).then(ad => sendJSONResponse(ad, res))
-
-const buckets = (req, res, index) => index.BUCKETS(
-  ...JSON.parse(params(req.url).q)
-).then(b => sendJSONResponse(b, res))
-
-const documentCount = (req, res, index) => index.DOCUMENT_COUNT().then(
-  td => sendResponse(td + '', res, 'text/plain')
-)
-
-const created = (req, res, index) => index.CREATED().then(
-  c => sendResponse(c + '', res, 'text/plain')
-)
-
-const lastUpdated = (req, res, index) => index.LAST_UPDATED().then(
-  lu => sendResponse(lu + '', res, 'text/plain')
-)
-
-const query = (req, res, index) => index.QUERY(
-  params(req.url).q,
-  JSON.parse(params(req.url).ops)
-).then(r => sendJSONResponse(r, res))
-
-// default ("page not found?")
-const def = (req, res) => {
+// ("404: page not found")
+const _404 = (req, res) => {
   res.setHeader("Content-Type", "text/html; charset=utf-8")
   res.writeHead(404)
   res.end('<html>nothing here bro!</html>')
 }
-
-
-const sendResponse = (body, res, type) => {
-  res.setHeader("Content-Type", type + '; charset=utf-8')
-  res.writeHead(200)
-  res.end(body)
-}
-
-const sendFileResponse = (name, res) => sendResponse(
-  fs.readFileSync(path.resolve(__dirname + '/../www_root' + name)) + '',
-  res,
-  mime.getType(name)
-)
 
 const files = dirs => {
   const getFilesInDir = name => fs.readdirSync(
@@ -85,49 +23,66 @@ const files = dirs => {
   return dirs.map(d => getFilesInDir(d)).flat()
 }
 
+const sendFileResponse = (name, res) => sendResponse(
+  fs.readFileSync(path.resolve(__dirname + '/../www_root' + name)) + '',
+  res,
+  mime.getType(name)
+)
+
+const sendResponse = (body, res, type) => {
+  res.setHeader("Content-Type", type + '; charset=utf-8')
+  res.writeHead(200)
+  res.end(body)
+}
+
 //create a server object:
 si({ name: 'norch-data' }).then(index =>
   http.createServer((req, res) => {
 
     const fileDirs = [ '/', '/openapi/' ]
+    const idx = Index(index, sendResponse)
     const pathname = url.parse(req.url).pathname
     
+    console.log(pathname)
+    
     // Files
-    if (files(fileDirs).includes(pathname)) return sendFileResponse(pathname, res)
+    if (files(fileDirs).includes(pathname))
+      return sendFileResponse(pathname, res)
 
     // Files (default to index.html when only directory is specified)
-    if (fileDirs.includes(pathname)) return sendFileResponse(pathname + 'index.html', res)
- 
+    if (fileDirs.includes(pathname))
+      return sendFileResponse(pathname + 'index.html', res)
+
     // API endpoints
-    if (pathname == '/all_documents') return allDocuments(req, res, index)
-    if (pathname == '/buckets') return buckets(req, res, index)
-    if (pathname == '/created') return created(req, res, index)
-    if (pathname == '/document-count') return documentCount(req, res, index)
-    if (pathname == '/get') return get(req, res)
-    if (pathname == '/last-updated') return lastUpdated(req, res, index)
-    if (pathname == '/query') return query(req, res, index)
-    if (pathname == '/put') return put(req, res, index)
+    return new Proxy({
+      '/all_documents': idx.allDocuments,
+      '/buckets': idx.buckets,
+      '/created': idx.created,
+      '/document-count': idx.documentCount,
+      '/get': idx.get,  // not sure if this should be here...
+      '/last-updated': idx.lastUpdated,
+      '/query': idx.query,
+      '/put': idx.put
+    }, {
+      get (target, property) {
+        return target[property]
+          ? target[property](req, res)
+          : _404(req, res)
+      }
+    })[pathname]
 
-
-
-// ALL_DOCUMENTS
-// BUCKETS
 // DELETE
-// CREATED
 // DICTIONARY
 // DOCUMENTS
 // DISTINCT
-// DOCUMENT_COUNT
 // EXPORT
 // FACETS
 // FIELDS
 // IMPORT
-// INDEX
 // MAX
 // MIN
 // PUT
 // PUT_RAW
     
-    return def(req, res)
   }).listen(3030) //the server object listens on port 8080 
 )
