4e53ddf07438cfa1c735090a522008d9f2f12fad
Fergus McDowall <fergusmcdowall@gmail.com>
2021-02-25 09:00:30 +0100

    latest

--- src/main.js
+++ src/main.js
@@ -1,10 +1,11 @@
+const API = require('./API.js')
 const fs = require('fs')
 const http = require('http')
 const mime = require('mime')
 const path = require('path')
 const querystring = require('querystring')
 const si = require('search-index')
-const Index = require('./index.js')
+const url = require('url')
 
 
 // ("404: page not found")
@@ -39,50 +40,62 @@ const sendResponse = (body, res, type) => {
 si({ name: 'norch-data' }).then(index =>
   http.createServer((req, res) => {
 
-    const fileDirs = [ '/', '/openapi/' ]
-    const idx = Index(index, sendResponse)
+    const api = API(index, sendResponse)
     const pathname = url.parse(req.url).pathname
-    
+    console.log(req.method)
     console.log(pathname)
     
-    // Files
-    if (files(fileDirs).includes(pathname))
-      return sendFileResponse(pathname, res)
-
-    // Files (default to index.html when only directory is specified)
-    if (fileDirs.includes(pathname))
-      return sendFileResponse(pathname + 'index.html', res)
-
-    // API endpoints
-    return new Proxy({
-      '/all_documents': idx.allDocuments,
-      '/buckets': idx.buckets,
-      '/created': idx.created,
-      '/document-count': idx.documentCount,
-      '/get': idx.get,  // not sure if this should be here...
-      '/last-updated': idx.lastUpdated,
-      '/query': idx.query,
-      '/put': idx.put
-    }, {
+    const routes = routes => new Proxy(routes, {
       get (target, property) {
         return target[property]
           ? target[property](req, res)
           : _404(req, res)
       }
-    })[pathname]
-
-// DELETE
-// DICTIONARY
-// DOCUMENTS
-// DISTINCT
-// EXPORT
-// FACETS
-// FIELDS
-// IMPORT
-// MAX
-// MIN
-// PUT
-// PUT_RAW
+    })
     
+    if (req.method == 'GET') {      
+      const fileDirs = [ '/', '/openapi/' ]
+
+      // Files
+      if (files(fileDirs).includes(pathname))
+        return sendFileResponse(pathname, res)
+
+      // Files (default to index.html when only directory is specified)
+      if (fileDirs.includes(pathname))
+        return sendFileResponse(pathname + 'index.html', res)
+
+      return routes({
+        '/buckets': api.buckets,
+        '/created': api.created,
+        '/document-count': api.documentCount,
+        '/documents/all': api.allDocuments,
+        '/documents/query': api.query,
+        '/get': api.get,  // not sure if this should be here...
+        '/last-updated': api.lastUpdated,
+        '/put': api.put
+      })[pathname]
+
+    }
+
+    
+    if (req.method == 'POST') {
+      return routes({
+        '/documents': api.put,
+      })[pathname]
+    }    
+                    
+                    // DELETE
+                    // DICTIONARY
+                    // DOCUMENTS
+                    // DISTINCT
+                    // EXPORT
+                    // FACETS
+                    // FIELDS
+                    // IMPORT
+                    // MAX
+                    // MIN
+                    // PUT
+                    // PUT_RAW
+                    
   }).listen(3030) //the server object listens on port 8080 
 )
