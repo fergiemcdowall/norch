6dd1972d78164a6aa67e1f03088b9a4e8944d601
Fergus McDowall <fergusmcdowall@gmail.com>
2021-03-08 20:39:30 +0100

    fixed bin file

--- src/main.js
+++ src/main.js
@@ -3,7 +3,6 @@ const fs = require('fs')
 const http = require('http')
 const mime = require('mime')
 const path = require('path')
-const querystring = require('querystring')
 const si = require('search-index')
 const url = require('url')
 
@@ -36,75 +35,77 @@ const sendResponse = (body, res, type) => {
   res.end(body)
 }
 
+
+const createServer = index => http.createServer((req, res) => {
+
+  // TODO: req/res should probably go here
+  const api = API(index, sendResponse)
+  const pathname = url.parse(req.url).pathname
+  console.log(req.method)
+  console.log(pathname)
+  
+  const routes = routes => new Proxy(routes, {
+    get (target, property) {
+      return target[property]
+        ? target[property](req, res)
+        : _404(req, res)
+    }
+  })
+  
+  if (req.method == 'GET') {      
+    const fileDirs = [ '/', '/openapi/' ]
+
+    // Files
+    if (files(fileDirs).includes(pathname))
+      return sendFileResponse(pathname, res)
+
+    // Files (default to index.html when only directory is specified)
+    if (fileDirs.includes(pathname))
+      return sendFileResponse(pathname + 'index.html', res)
+
+    return routes({
+      '/aggregate/buckets': api.buckets,
+      '/aggregate/facets': api.facets,
+      '/documents': api.documents,
+      '/documents/all': api.allDocuments,
+      '/documents/count': api.documentCount,
+      '/documents/query': api.query,
+      '/meta/created': api.created,
+      '/meta/fields': api.fields,
+      '/meta/last-updated': api.lastUpdated,
+      '/replicate': api.replicate,
+      '/values/dictionary': api.dictionary,
+      '/values/distinct': api.distinct,
+      '/values/max': api.max,
+      '/values/min': api.min,
+
+      '/get': api.get,  // not sure if this should be here...
+    })[pathname]
+
+  }
+  
+  if (req.method == 'POST') {
+    console.log('in POST ' + pathname)
+    return routes({
+      '/documents': api.put,
+      '/documents/raw': api.putRaw,
+      '/replicate': api.imprt,
+    })[pathname]
+  }
+
+  if (req.method == 'DELETE') {
+    return routes({
+      '/documents': api.del,
+      '/documents/flush': api.flush,
+    })[pathname]
+  }
+
+})
+
 //create a server object:
-si({
-  name: 'norch-data',
+module.exports = ops => si({
+  ...JSON.parse(ops.searchIndexOptions),
+  name: ops.norchHome,
   storeVectors: true
-}).then(index =>
-  http.createServer((req, res) => {
-
-    // TODO: req/res should probably go here
-    const api = API(index, sendResponse)
-    const pathname = url.parse(req.url).pathname
-    console.log(req.method)
-    console.log(pathname)
-    
-    const routes = routes => new Proxy(routes, {
-      get (target, property) {
-        return target[property]
-          ? target[property](req, res)
-          : _404(req, res)
-      }
-    })
-    
-    if (req.method == 'GET') {      
-      const fileDirs = [ '/', '/openapi/' ]
-
-      // Files
-      if (files(fileDirs).includes(pathname))
-        return sendFileResponse(pathname, res)
-
-      // Files (default to index.html when only directory is specified)
-      if (fileDirs.includes(pathname))
-        return sendFileResponse(pathname + 'index.html', res)
-
-      return routes({
-        '/aggregate/buckets': api.buckets,
-        '/aggregate/facets': api.facets,
-        '/documents': api.documents,
-        '/documents/all': api.allDocuments,
-        '/documents/count': api.documentCount,
-        '/documents/query': api.query,
-        '/meta/created': api.created,
-        '/meta/fields': api.fields,
-        '/meta/last-updated': api.lastUpdated,
-        '/replicate': api.replicate,
-        '/values/dictionary': api.dictionary,
-        '/values/distinct': api.distinct,
-        '/values/max': api.max,
-        '/values/min': api.min,
-
-        '/get': api.get,  // not sure if this should be here...
-      })[pathname]
-
-    }
-
-    
-    if (req.method == 'POST') {
-      console.log('in POST ' + pathname)
-      return routes({
-        '/documents': api.put,
-        '/documents/raw': api.putRaw,
-        '/replicate': api.imprt,
-      })[pathname]
-    }
-
-    if (req.method == 'DELETE') {
-      return routes({
-        '/documents': api.del,
-        '/documents/flush': api.flush,
-      })[pathname]
-    }
-
-  }).listen(3030) //the server object listens on port 8080 
-)
+}).then(createServer)
+  .then(server => server.listen(ops.port))
