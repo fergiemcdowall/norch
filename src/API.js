13be56bafe052cb5bb1f3b05bcb7a34b11778cb0
Fergus McDowall <fergus@uio.no>
2022-11-07 14:45:19 +0100

    latest

--- src/API.js
+++ src/API.js
@@ -1,139 +1,140 @@
 const url = require('url')
 
 module.exports = (index, sendResponse) => {
-
-  const params = _url => url.parse(_url, {
-    parseQueryString: true
-  }).query
+  const params = _url =>
+    url.parse(_url, {
+      parseQueryString: true
+    }).query
 
   const sendJSONResponse = (body, res) => {
-    res.setHeader("Content-Type", 'application/json; charset=utf-8')
+    res.setHeader('Content-Type', 'application/json; charset=utf-8')
     res.writeHead(200)
     res.end(JSON.stringify(body, null, 2))
   }
-  
+
   // *************
 
-
-  // TODO: do we still need the formField hack?
-  // curl -H "Content-Type: application/json" --data @testdata.json http://localhost:8081/put
-  // if ?form=[fieldName] then read from that form field
-  const put = (req, res) => {
-    const formField = url.parse(req.url, {
-      parseQueryString: true
-    }).query.form
-    var body = ''
-    req.on('data', d => body += d.toString())
-    req.on('end', () => index.PUT(JSON.parse(
-      formField
-        ? querystring.decode(body)[formField]
-        : body
-    )).then(
-      idxRes => sendJSONResponse(idxRes, res)
-    ))
-  }
-
-  const imprt = (req, res) => {
-    console.log('in imprt')
-    var body = ''
-    req.on('data', d => body += d.toString())
-    req.on('end', () => index.IMPORT(JSON.parse(body)).then(
-      idxRes => sendJSONResponse(idxRes, res)
-    ))
-  }
-
-  const putRaw = (req, res) => {
-    var body = ''
-    req.on('data', d => body += d.toString())
-    req.on('end', () => index.PUT_RAW(JSON.parse(body)).then(
-      idxRes => sendJSONResponse(idxRes, res)
-    ))
-  }
-  
-  const del = (req, res) => index.DELETE(
-    [params(req.url).ids].flat()
-  ).then(idxRes => sendJSONResponse(idxRes, res))
-
-  const flush = (req, res) => index.FLUSH()
-        .then(idxRes => sendJSONResponse(idxRes, res))
+  const del = (req, res) =>
+    index
+      .DELETE([params(req.url).ids].flat())
+      .then(idxRes => sendJSONResponse(idxRes, res))
 
   const get = (req, res) => res.end('This is GET yo!')
 
-  const allDocuments = (req, res) => index.ALL_DOCUMENTS(
-    +params(req.url).limit || undefined
-  ).then(ad => sendJSONResponse(ad, res))
+  const lastUpdated = (req, res) =>
+    index.LAST_UPDATED().then(lu => sendResponse(lu + '', res, 'text/plain'))
 
-  const buckets = (req, res) => index.BUCKETS(
-    ...JSON.parse('[' + params(req.url).q + ']')
-  ).then(b => sendJSONResponse(b, res))
-
-  const documents = (req, res) => index.DOCUMENTS(
-    params(req.url).ids
-  ).then(b => sendJSONResponse(b, res))
-
-  const dictionary = (req, res) => index.DICTIONARY()
-        .then(d => sendJSONResponse(d, res))  
-
-  const distinct = (req, res) => index.DISTINCT()
-        .then(d => sendJSONResponse(d, res))
-  
-  const facets = (req, res) => index.FACETS(
-    JSON.parse(params(req.url).q)
-  ).then(b => sendJSONResponse(b, res))
-  
-  const documentCount = (req, res) => index.DOCUMENT_COUNT().then(
-    td => sendResponse(td + '', res, 'text/plain')
-  )
-
-  const created = (req, res) => index.CREATED().then(
-    c => sendResponse(c + '', res, 'text/plain')
-  )
-
-  const lastUpdated = (req, res) => index.LAST_UPDATED().then(
-    lu => sendResponse(lu + '', res, 'text/plain')
-  )
-
-  const query = (req, res) => index.QUERY(
-    JSON.parse(params(req.url).query || '{}'),
-    JSON.parse(params(req.url).ops || '{}')
-  ).then(r => sendJSONResponse(r, res))
-
-  const replicate = (req, res) => index.EXPORT().then(
-    exp => sendJSONResponse(exp, res)
-  )
-
-  const max = (req, res) => index.MAX(
-    params(req.url).field
-  ).then(m => sendJSONResponse(m, res))
-
-  const min = (req, res) => index.MIN(
-    params(req.url).field
-  ).then(m => sendJSONResponse(m, res))
-
-  const fields = (req, res) => index.FIELDS()
-        .then(f => sendJSONResponse(f, res))
-  
   return {
-    allDocuments: allDocuments,
-    buckets: buckets,
-    created: created,
-    del: del,
-    dictionary: dictionary,
-    distinct: distinct,
-    documentCount: documentCount,
-    documents: documents,
-    facets: facets,
-    fields: fields,
-    flush: flush,
-    get: get,
-    imprt: imprt,
-    lastUpdated: lastUpdated,
-    max: max,
-    min: min,
-    put: put,
-    putRaw: putRaw,
-    query: query,
-    replicate: replicate
-  }
+    ALL_DOCUMENTS: (req, res) =>
+      index
+        .ALL_DOCUMENTS(+params(req.url).limit || undefined)
+        .then(ad => sendJSONResponse(ad, res)),
 
+    BUCKETS: (req, res) =>
+      index
+        .BUCKETS(...JSON.parse('[' + params(req.url).q + ']'))
+        .then(b => sendJSONResponse(b, res)),
+
+    CREATED: (req, res) =>
+      index.CREATED().then(c => sendResponse(c + '', res, 'text/plain')),
+
+    del: del,
+
+    DICTIONARY: (req, res) =>
+      index.DICTIONARY().then(d => sendJSONResponse(d, res)),
+
+    DISTINCT: (req, res) =>
+      index.DISTINCT().then(d => sendJSONResponse(d, res)),
+
+    DOCUMENT_COUNT: (req, res) =>
+      index
+        .DOCUMENT_COUNT()
+        .then(td => sendResponse(td + '', res, 'text/plain')),
+
+    // TODO: DOCUMENTS doesnt seem to work for docs with ID of type int
+    DOCUMENTS: (req, res) =>
+      index.DOCUMENTS(params(req.url).ids).then(b => sendJSONResponse(b, res)),
+
+    EXPORT: (req, res) =>
+      index.EXPORT().then(exp => sendJSONResponse(exp, res)),
+
+    FACETS: (req, res) =>
+      index
+        .FACETS(JSON.parse(params(req.url).q))
+        .then(b => sendJSONResponse(b, res)),
+
+    FIELDS: (req, res) => index.FIELDS().then(f => sendJSONResponse(f, res)),
+
+    FLUSH: (req, res) =>
+      index.FLUSH().then(idxRes => sendJSONResponse(idxRes, res)),
+
+    get: get,
+
+    IMPORT: (req, res) => {
+      console.log('in imprt')
+      var body = ''
+      req.on('data', d => (body += d.toString()))
+      req.on('end', () =>
+        index
+          .IMPORT(JSON.parse(body))
+          .then(idxRes => sendJSONResponse(idxRes, res))
+      )
+    },
+
+    lastUpdated: lastUpdated,
+
+    MAX: (req, res) =>
+      index
+        .MAX(JSON.parse(params(req.url).TOKEN))
+        .then(m => sendJSONResponse(m, res)),
+
+    MIN: (req, res) =>
+      index
+        .MIN(JSON.parse(params(req.url).TOKEN))
+        .then(m => sendJSONResponse(m, res)),
+
+    // TODO: do we still need the formField hack?
+    // curl -H "Content-Type: application/json" --data @testdata.json http://localhost:8081/put
+    // if ?form=[fieldName] then read from that form field
+    PUT: (req, res) => {
+      const formField = url.parse(req.url, {
+        parseQueryString: true
+      }).query.form
+      var body = ''
+      req.on('data', d => (body += d.toString()))
+      req.on('end', () =>
+        index
+          .PUT(
+            JSON.parse(formField ? querystring.decode(body)[formField] : body)
+          )
+          .then(idxRes => sendJSONResponse(idxRes, res))
+      )
+    },
+
+    PUT_RAW: (req, res) => {
+      var body = ''
+      req.on('data', d => (body += d.toString()))
+      req.on('end', () =>
+        index
+          .PUT_RAW(JSON.parse(body))
+          .then(idxRes => sendJSONResponse(idxRes, res))
+      )
+    },
+
+    QUERY: (req, res) =>
+      index
+        .QUERY(
+          JSON.parse(params(req.url).TOKEN),
+          JSON.parse(params(req.url).ops || '{}')
+        )
+        .then(r => sendJSONResponse(r, res)),
+
+    SEARCH: (req, res) =>
+      index
+        .SEARCH(
+          JSON.parse(params(req.url).TOKEN),
+          JSON.parse(params(req.url).ops || '{}')
+        )
+        .then(r => sendJSONResponse(r, res))
+  }
 }
--- src/main.js
+++ src/main.js
@@ -6,106 +6,103 @@ const path = require('path')
 const si = require('search-index')
 const url = require('url')
 
-
 // ("404: page not found")
 const _404 = (req, res) => {
-  res.setHeader("Content-Type", "text/html; charset=utf-8")
+  res.setHeader('Content-Type', 'text/html; charset=utf-8')
   res.writeHead(404)
   res.end('<html>nothing here bro!</html>')
 }
 
 const files = dirs => {
-  const getFilesInDir = name => fs.readdirSync(
-    '/Users/fergie/projects/norch/www_root' + name, {
-      withFileTypes: true
-    }
-  ).filter(dirent => dirent.isFile()).map(f => name + f.name)
+  const getFilesInDir = name =>
+    fs
+      .readdirSync('/Users/fergie/projects/norch/www_root' + name, {
+        withFileTypes: true
+      })
+      .filter(dirent => dirent.isFile())
+      .map(f => name + f.name)
   return dirs.map(d => getFilesInDir(d)).flat()
 }
 
-const sendFileResponse = (name, res) => sendResponse(
-  fs.readFileSync(path.resolve(__dirname + '/../www_root' + name)) + '',
-  res,
-  mime.getType(name)
-)
+const sendFileResponse = (name, res) =>
+  sendResponse(
+    fs.readFileSync(path.resolve(__dirname + '/../www_root' + name)) + '',
+    res,
+    mime.getType(name)
+  )
 
 const sendResponse = (body, res, type) => {
-  res.setHeader("Content-Type", type + '; charset=utf-8')
+  res.setHeader('Content-Type', type + '; charset=utf-8')
   res.writeHead(200)
   res.end(body)
 }
 
+const createServer = index =>
+  http.createServer((req, res) => {
+    // TODO: req/res should probably go here
+    const api = API(index, sendResponse)
+    const pathname = url.parse(req.url).pathname
+    console.log(req.method)
+    console.log(pathname)
 
-const createServer = index => http.createServer((req, res) => {
+    const routes = routes =>
+      new Proxy(routes, {
+        get(target, property) {
+          return target[property] ? target[property](req, res) : _404(req, res)
+        }
+      })
 
-  // TODO: req/res should probably go here
-  const api = API(index, sendResponse)
-  const pathname = url.parse(req.url).pathname
-  console.log(req.method)
-  console.log(pathname)
-  
-  const routes = routes => new Proxy(routes, {
-    get (target, property) {
-      return target[property]
-        ? target[property](req, res)
-        : _404(req, res)
+    if (req.method == 'GET') {
+      const fileDirs = ['/', '/openapi/']
+
+      // Files
+      if (files(fileDirs).includes(pathname))
+        return sendFileResponse(pathname, res)
+
+      // Files (default to index.html when only directory is specified)
+      if (fileDirs.includes(pathname))
+        return sendFileResponse(pathname + 'index.html', res)
+
+      return routes({
+        '/ALL_DOCUMENTS': api.ALL_DOCUMENTS,
+        '/BUCKETS': api.BUCKETS,
+        '/CREATED': api.CREATED,
+        '/DICTIONARY': api.DICTIONARY,
+        '/DISTINCT': api.DISTINCT,
+        '/DOCUMENTS': api.DOCUMENTS,
+        '/DOCUMENT_COUNT': api.DOCUMENT_COUNT,
+        '/EXPORT': api.EXPORT,
+        '/FACETS': api.FACETS,
+        '/FIELDS': api.FIELDS,
+        '/MAX': api.MAX,
+        '/MIN': api.MIN,
+        '/QUERY': api.QUERY,
+        '/SEARCH': api.SEARCH
+      })[pathname]
+    }
+
+    if (req.method == 'POST') {
+      return routes({
+        '/PUT': api.PUT,
+        '/PUT_RAW': api.PUT_RAW,
+        '/IMPORT': api.IMPORT
+      })[pathname]
+    }
+
+    if (req.method == 'DELETE') {
+      return routes({
+        '/DELETE': api.DELETE,
+        '/FLUSH': api.FLUSH
+      })[pathname]
     }
   })
-  
-  if (req.method == 'GET') {      
-    const fileDirs = [ '/', '/openapi/' ]
-
-    // Files
-    if (files(fileDirs).includes(pathname))
-      return sendFileResponse(pathname, res)
-
-    // Files (default to index.html when only directory is specified)
-    if (fileDirs.includes(pathname))
-      return sendFileResponse(pathname + 'index.html', res)
-
-    return routes({
-      '/aggregate/buckets': api.buckets,
-      '/aggregate/facets': api.facets,
-      '/documents': api.documents,
-      '/documents/all': api.allDocuments,
-      '/documents/count': api.documentCount,
-      '/documents/query': api.query,
-      '/meta/created': api.created,
-      '/meta/fields': api.fields,
-      '/meta/last-updated': api.lastUpdated,
-      '/replicate': api.replicate,
-      '/values/dictionary': api.dictionary,
-      '/values/distinct': api.distinct,
-      '/values/max': api.max,
-      '/values/min': api.min,
-
-      '/get': api.get,  // not sure if this should be here...
-    })[pathname]
-
-  }
-  
-  if (req.method == 'POST') {
-    console.log('in POST ' + pathname)
-    return routes({
-      '/documents': api.put,
-      '/documents/raw': api.putRaw,
-      '/replicate': api.imprt,
-    })[pathname]
-  }
-
-  if (req.method == 'DELETE') {
-    return routes({
-      '/documents': api.del,
-      '/documents/flush': api.flush,
-    })[pathname]
-  }
-
-})
 
 //create a server object:
-module.exports = ops => si({
-  ...JSON.parse(ops.searchIndexOptions),
-  name: ops.norchHome,
-  storeVectors: true
-}).then(createServer)
-  .then(server => server.listen(ops.port))
+module.exports = ops =>
+  si({
+    ...JSON.parse(ops.searchIndexOptions),
+    name: ops.norchHome,
+    storeVectors: true
+  })
+    .then(createServer)
+    .then(server => server.listen(ops.port))
